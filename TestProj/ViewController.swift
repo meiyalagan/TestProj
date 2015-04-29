c790720e90591c3cea43481325ec0e37aa174ced
Meiyalagan R <meiyalagan_r@Meiyalagan-r.local>
2015-04-29 10:41:19 +0530

    Initial Commit

---
+++ TestProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProj
+//
+//  Created by Meiyalagan R on 29/04/15.
+//  Copyright (c) 2015 Meiyalagan R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
